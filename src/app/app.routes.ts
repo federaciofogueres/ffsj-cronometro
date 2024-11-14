d9dd4fd26dcb6c3324e0f8476a50996c2fbe5cb9
Fran García <fgarciadevelop@gmail.com>
2024-11-14 15:15:57 +0100

    feat: añadidos algunos servicios, header y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './components/login/login.component';
 import { TimerComponent } from './components/timer/timer.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: TimerComponent },
     // { path: 'formulario', component: FormularioComponent, canActivate: [AuthGuard] },
-    { path: 'admin', component: AdminComponent },
+    { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: '**', component: TimerComponent },
     // { path: '**', component: HomeComponent, canActivate: [AuthGuard] },
