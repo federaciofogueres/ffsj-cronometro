df48135e0aee283f76f69bb94d6606b39b9e36d5
Fran García <fgarciadevelop@gmail.com>
2024-11-15 13:13:54 +0100

    feat: crear sesiones ok

--- src/app/components/sesion/sesion.component.ts
+++ src/app/components/sesion/sesion.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { Asociacion, InlineResponse200, SesionesService, Session, SessionResponse } from '../../../api';
+import { MatSelectModule } from '@angular/material/select';
+import { Asociacion, InlineResponse200, SesionesService, Session, SessionResponse, TypeSesionService, TypeSession, TypeSessionResponse } from '../../../api';
 import { ChoreService } from '../../services/chore.service';
 import { Action } from '../asociacion/asociacion.component';
 import { ParticipantesComponent } from '../participantes/participantes.component';
@@ -19,29 +22,40 @@ import { ParticipantesComponent } from '../participantes/participantes.component
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     ParticipantesComponent
   ],
   templateUrl: './sesion.component.html',
   styleUrl: './sesion.component.scss'
 })
 export class SesionComponent {
+  FormControl = FormControl;
   @Input() session!: Session;
   @Output() onBack: EventEmitter<void> = new EventEmitter();
 
   public newSessionForm!: FormGroup;
+
+
   loading: boolean = true;
   action: Action = 'Crear';
 
   manageVarticipantesShow: boolean = false;
   asociacionesRelatedToSesion: Asociacion[] = [];
 
+  tiposSesiones: TypeSession[] = [];
+
   constructor(
     private fb: FormBuilder,
     private choreService: ChoreService,
     private sessionService: SesionesService,
+    private typeSesionService: TypeSesionService
   ) { }
 
   ngOnInit() {
+    this.getTypeSesion();
     this.choreService.asociacionesSelectedsObservable.subscribe(res => {
       this.asociacionesRelatedToSesion = res;
     })
@@ -71,10 +85,23 @@ export class SesionComponent {
 
   loadForm() {
     this.newSessionForm = this.fb.group({
-      id: new FormControl(this.session.id, Validators.required),
+      id: new FormControl(this.session.id, []),
       session_title: new FormControl(this.session.session_title, [Validators.required]),
       type: new FormControl(this.session.type, Validators.required),
+      session_date: new FormControl('', Validators.required),
+      session_time: new FormControl('', Validators.required)
     });
+    this.loading = false;
+  }
+
+  getTypeSesion() {
+    this.typeSesionService.getAllTypeSesion().subscribe({
+      next: (response: TypeSessionResponse) => {
+        console.log({ response });
+        this.tiposSesiones = response.types!;
+
+      }
+    })
   }
 
   processAction() {
@@ -82,8 +109,8 @@ export class SesionComponent {
       let session: Session = {
         id: this.newSessionForm.controls['id'].value,
         session_title: this.newSessionForm.controls['session_title'].value,
-        type: parseInt(this.newSessionForm.controls['type'].value),
-        type_normalized: '',
+        type: parseInt(this.newSessionForm.controls['type'].value.id),
+        type_normalized: this.newSessionForm.controls['type'].value.type_normalized,
         participants: this.asociacionesRelatedToSesion
       }
       console.log(session);
@@ -98,6 +125,8 @@ export class SesionComponent {
   }
 
   create(session: Session): void {
+    session.id = '0';
+    session.session_title = `${session.type_normalized} - ${session.session_title}`
     this.sessionService.createSesion(session).subscribe((res: InlineResponse200) => {
       if (res.status?.code === '200') {
         this.back();
@@ -134,4 +163,24 @@ export class SesionComponent {
   back() {
     this.onBack.emit();
   }
+
+  parseFecha() {
+    const date = this.newSessionForm.get('session_date')?.value;
+    const time = this.newSessionForm.get('session_time')?.value;
+
+    if (date && time) {
+      const dayNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+      const monthNames = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+
+      const dayName = dayNames[date.getDay()];
+      const day = date.getDate();
+      const monthName = monthNames[date.getMonth()];
+      const year = date.getFullYear();
+      const [hours, minutes] = time.split(':');
+
+      const formattedDate = `${dayName} ${day} ${monthName} ${year} - ${hours}:${minutes}h`;
+      this.newSessionForm.get('session_title')?.setValue(formattedDate);
+    }
+  }
+
 }
